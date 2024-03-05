adcb6c6f40156a596099b3dd7e3bd82dae03c6ce
MinWook6457 <minuk6457@gmail.com>
2024-03-05 16:11:59 +0900

    NEXT 라우팅 및 client component 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Tomato(){
-    return <h1>Hello Next</h1>
+/* page.tsx가 없다면 폴더 경로는 그저 경로의 일부분 */
+/* 페이지에 원하는 화면을 띄우기 위해서는 page.tsx or page.jsx가 존재해야함.    */
+
+import Navigation from "../components/navigation";
+
+export default function Home(){
+    return (
+    <div>
+        <Navigation />
+            <h1>Hello</h1>
+    </div>
+    )
 }
